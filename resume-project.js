e185fced7cc3d5acc4944961d23e4523ec52815e
Luke Alexander The Great And Almighty <luke.alexander@students.snow.edu>
2024-11-07 15:35:30 -0700

    Updated

--- resume-project.js
+++ resume-project.js
@@ -1,31 +1,30 @@
+const elementButton1 = document.getElementById("Button1");
 
-    const elementButton1 = document.getElementById("Button1");
+elementButton1.style.cursor = "pointer";
+elementButton1.addEventListener("click", function(){
+    window.location.href = "resume-profile-project.html";
+});
 
-    elementButton1.style.cursor = "pointer";
-    elementButton1.addEventListener("click", function(){
-        window.location.href = "https://www.w3schools.com/jsref/jsref_link.asp";
-    });
 
-    
-    const elementButton2 = document.getElementById("Button2");
+const elementButton2 = document.getElementById("Button2");
 
-    elementButton2.style.cursor = "pointer";
-    elementButton2.addEventListener("click", function(){
-        window.location.href = "https://www.w3schools.com/jsref/jsref_link.asp";
-    });
+elementButton2.style.cursor = "pointer";
+elementButton2.addEventListener("click", function(){
+    window.location.href = "https://www.w3schools.com/jsref/jsref_link.asp";
+});
 
-    
-    const elementButton3 = document.getElementById("Button3");
 
-    elementButton3.style.cursor = "pointer";
-    elementButton3.addEventListener("click", function(){
-        window.location.href = "https://www.w3schools.com/jsref/jsref_link.asp";
-    });
+const elementButton3 = document.getElementById("Button3");
 
-    
-    const elementButton4 = document.getElementById("Button4");
+elementButton3.style.cursor = "pointer";
+elementButton3.addEventListener("click", function(){
+    window.location.href = "https://www.w3schools.com/jsref/jsref_link.asp";
+});
 
-    elementButton4.style.cursor = "pointer";
-    elementButton4.addEventListener("click", function(){
-        window.location.href = "https://www.w3schools.com/jsref/jsref_link.asp";
-    });
+
+const elementButton4 = document.getElementById("Button4");
+
+elementButton4.style.cursor = "pointer";
+elementButton4.addEventListener("click", function(){
+    window.location.href = "https://www.w3schools.com/jsref/jsref_link.asp";
+});

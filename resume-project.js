c199e39e777a1c8db80898b02230aea0982bf47a
Luke Alexander The Great And Almighty <luke.alexander@students.snow.edu>
2024-11-07 15:25:08 -0700

    Inital Transfer- Not Updated

---
+++ resume-project.js
@@ -0,0 +1,31 @@
+
+    const elementButton1 = document.getElementById("Button1");
+
+    elementButton1.style.cursor = "pointer";
+    elementButton1.addEventListener("click", function(){
+        window.location.href = "https://www.w3schools.com/jsref/jsref_link.asp";
+    });
+
+    
+    const elementButton2 = document.getElementById("Button2");
+
+    elementButton2.style.cursor = "pointer";
+    elementButton2.addEventListener("click", function(){
+        window.location.href = "https://www.w3schools.com/jsref/jsref_link.asp";
+    });
+
+    
+    const elementButton3 = document.getElementById("Button3");
+
+    elementButton3.style.cursor = "pointer";
+    elementButton3.addEventListener("click", function(){
+        window.location.href = "https://www.w3schools.com/jsref/jsref_link.asp";
+    });
+
+    
+    const elementButton4 = document.getElementById("Button4");
+
+    elementButton4.style.cursor = "pointer";
+    elementButton4.addEventListener("click", function(){
+        window.location.href = "https://www.w3schools.com/jsref/jsref_link.asp";
+    });
